823fb4f03bd45761a4c851fd465c050633f123d9
Suheyl Turac <skbsturac@live.com>
2018-08-16 23:17:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = "74"
 module.exports = { name, height
 }
 if (name === "Susan") {
-  console.log("Expected: 70, Received: "+index.height)
+  console.log()
 } else {
   
 console.log("Expected the name to be Susan")
